4688eb174e691004d057750b3cc3ecb654a0efa0
Jacob Elder <jacob@boundless.com>
2013-10-03 23:14:32 -0400

    timeago doesn't work on new records?

--- public/1.js
+++
@@ -1,1 +0,0 @@
-receive({"door":1,"state":"closed","hostname":"jeldermbp.local","timestamp":"2013-10-04T01:40:49Z"})
--- public/2.js
+++
@@ -1,1 +0,0 @@
-receive({"door":2,"state":"closed","last_duration":"2013-10-03 21:40:45 -0400","hostname":"jeldermbp.local","timestamp":"2013-10-04T01:41:08Z"})
