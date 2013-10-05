14b62b6eded7c1e95cd872542b078847c8c2843a
Jacob Elder <jacob@boundless.com>
2013-10-05 15:10:16 -0400

    Add logging

--- lib/door_agent/pusher_worker.rb
+++ lib/door_agent/pusher_worker.rb
@@ -2,8 +2,13 @@ class DoorAgent
   class PusherWorker
     include SuckerPunch::Job
 
+    CHANNEL = 'doors'
+    EVENT = 'state_change'
+
     def perform(message)
-      Pusher.trigger('doors', 'state_change', message)
+      @@logger ||= ::Logger.new(STDERR)
+      @@logger.info "Pusher.trigger(#{CHANNEL.inspect}, #{EVENT.inspect}, #{message.inspect})"
+      Pusher.trigger(CHANNEL, EVENT, message)
     end
   end
 end
